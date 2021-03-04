e97fe6b55241ac6f9e8e3e9f37600d923f711867
Loweny <riquier.thomas35@gmail.com>
2021-03-04 14:51:07 +0100

    MonProfil

--- src/Controller/MonProfilController.php
+++ src/Controller/MonProfilController.php
@@ -9,7 +9,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
+
 
 class MonProfilController extends AbstractController
 {
@@ -19,46 +22,44 @@ class MonProfilController extends AbstractController
     public function MonProfil(Request $request, EntityManager $em): Response
     {
 
-//        $participant = new Participant();
-//        $form = $this->createForm(ParticipantProfilFormType::class, $participant);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//        //    /** @var PhotoFile $photoFile */
-//            $photoFile = $form->get('brochure')->getData();
-//
-//            // this condition is needed because the 'brochure' field is not required
-//            // so the PDF file must be processed only when a file is uploaded
-//            if ($photoFile) {
-//                $originalFilename = pathinfo($photoFile->getClientOriginalName(), PATHINFO_FILENAME);
-//                // this is needed to safely include the file name as part of the URL
-//                $safeFilename = $slugger->slug($originalFilename);
-//                $newFilename = $safeFilename.'-'.uniqid().'.'.$photoFile->guessExtension();
-//
-//                // Move the file to the directory where brochures are stored
-//                try {
-//                    $photoFile->move(
-//                        $this->getParameter('Photo_directory'),
-//                        $newFilename
-//                    );
-//                } catch (FileException $e) {
-//                    // ... handle exception if something happens during file upload
-//                }
-//
-//                // updates the 'photoFilename' property to store the png /jpeg file name
-//                // instead of its contents
-//                $participant->setBrochureFilename($newFilename);
-//            }
-//
-//            // ... persist the $product variable or any other work
-//
-//            //add sql request update. Maybe check for the new participant to be an existant object and not a new one
-//
-//            return $this->redirectToRoute('mon_profil');
-//        }
+        $participant = new Participant();
+        $form = $this->createForm(ParticipantProfilFormType::class, $participant);
+        $form->handleRequest($request);
 
-        return $this->render('mon_profil/Profil.html.twig');
-//            'form' => $form->createView(),
-  //      ]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $photoFile */
+            $photoFile = $form->get('brochure')->getData();
+
+            if ($photoFile) {
+                $originalFilename = pathinfo($photoFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$photoFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $photoFile->move(
+                        $this->getParameter('photo_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $participant->setPhoto($newFilename);
+            }
+
+            // ... persist the $product variable or any other work
+
+            //add sql request update. Maybe check for the new participant to be an existant object and not a new one
+
+            return $this->redirectToRoute('mon_profil');
+        }
+
+        return $this->render('mon_profil/Profil.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
