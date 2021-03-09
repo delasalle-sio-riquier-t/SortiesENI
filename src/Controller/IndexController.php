04f58a9a510c1c66876b8888731291ece0b86f62
Suu <dinomax@live.fr>
2021-03-09 14:59:36 +0100

    debut index

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class IndexController extends AbstractController
 {
@@ -14,9 +17,11 @@ class IndexController extends AbstractController
     public function index(): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
+        $repository = $this->getDoctrine()->getRepository(Sortie::class);
+        $sorties = $repository->findAll();
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'sorties' => $sorties,
         ]);
     }
 }
